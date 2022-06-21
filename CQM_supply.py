341110015b92e08541957bc737c61b9776f2e4e7
weichen xie <xiew@clarkson.edu>
2022-06-21 15:41:55 +0000

    Mdf CQM_supply.py completely solved

--- CQM_supply.py
+++ CQM_supply.py
@@ -13,7 +13,7 @@ V = [{0,2,3,4,9}, {0,9,2}, {0,2,3,4}, {0,9,2,3}, {0,2,3,4,9}]
 cqm = CQM()
 
 # Create Binary variables
-bin_variables = [Binary(i) for i in range(len(U))]
+bin_variables = [Binary(i) for i in range(len(V))]
 
 # -------------- Objective Function ------------------
 # minimize total number of suppliers
@@ -22,3 +22,23 @@ cqm.set_objective(quicksum(bin_variables[i] for i in range(len(U))))
 
 # -------------- Constraints -----------------
 # suppliers should cover all items
+for a in range(len(U)):
+    cqm.add_constraint( quicksum(int(U[a] in V[i])*bin_variables[i] for i in range(len(V))) >= 1,
+                        label = 'cover item {:d}'.format(a) )
+
+# -------------- Submit to CQM sampler ---------------
+cqm_sampler = LeapHybridCQMSampler()
+sampleset = cqm_sampler.sample_cqm(cqm, label = 'Supply Demo')
+
+# -------------- Process the results ---------------
+print('CQM Solution:')
+#feasible_sols = np.where(sampleset.record.is_feasible == True)
+feasible_sols = sampleset.filter(lambda row: row.is_feasible == True)
+if not len(feasible_sols):
+    print("\nNo feasible solution found.\n")
+else:
+    sol = [int(feasible_sols.first.sample[i]) for i in range(len(V))]
+    print(sol)
+    print('There are {:d} supplier selected.'.format(int(quicksum(sol))))
+    suppliers = [f'suppiler{i}' for i in np.where(sol)[0]]
+    print('Selected Suppliers:', suppliers)
