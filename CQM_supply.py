285c21e50e7caab829366ca71427edf4651ac75f
weichen xie <xiew@clarkson.edu>
2022-06-26 01:51:26 +0000

    Add CQM_supply.ipynb

--- CQM_supply.py
+++ CQM_supply.py
@@ -6,34 +6,34 @@ import numpy as np
 # items (e.g. [0,2,3,4,9])
 U = list(set(np.random.randint(10, size=(10))))
 # suppliers (e.g. [{0,2,3,4,9}, {0,9,2}, {0,2,3,4}, {0,9,2,3}, {0,2,3,4,9}])
-V = [set(U[i] for i in np.random.randint(len(U), size=(8))) for j in range(5)]
+S = [set(U[i] for i in np.random.randint(len(U), size=(8))) for j in range(5)]
 
 # Print set up
 print('------------- Problem set up -------------')
 print('The universe is',U)
 print('Number of elements in the universe: {:d}'.format(len(U)))
 
-print('There are {:d} collections:'.format(len(V)))
-for i in range(len(V)):
-    print('Supplier{:d}:'.format(i), V[i])
-print('Number of sets: N={:d}'.format(len(V)))
+print('There are {:d} collections:'.format(len(S)))
+for j in range(len(S)):
+    print('Supplier{:d}:'.format(j), S[j])
+print('Number of sets: N={:d}'.format(len(S)))
 
 # Build CQM
 cqm = CQM()
 
 # Create Binary variables
-bin_variables = [Binary(i) for i in range(len(V))]
+y = [Binary(j) for j in range(len(S))]
 
 # -------------- Objective Function ------------------
 # minimize total number of suppliers
 # Add obj to CQM
-cqm.set_objective(quicksum(bin_variables[i] for i in range(len(V))))
+cqm.set_objective(quicksum(y[j] for j in range(len(S))))
 
 # -------------- Constraints -----------------
 # suppliers should cover all items
-for a in range(len(U)):
-    cqm.add_constraint( quicksum(int(U[a] in V[i])*bin_variables[i] for i in range(len(V))) >= 1,
-                        label = 'cover item {:d}'.format(a) )
+for i in range(len(U)):
+    cqm.add_constraint( quicksum(int(U[i] in S[j])*y[j] for j in range(len(S))) >= 1,
+                        label = 'cover item {:d}'.format(i) )
 
 # -------------- Submit to CQM sampler ---------------
 cqm_sampler = LeapHybridCQMSampler()
@@ -45,7 +45,7 @@ feasible_sols = sampleset.filter(lambda row: row.is_feasible == True)
 if not len(feasible_sols):
     print("\nNo feasible solution found.\n")
 else:
-    sol = [int(feasible_sols.first.sample[i]) for i in range(len(V))]
+    sol = [int(feasible_sols.first.sample[i]) for i in range(len(S))]
     print(sol)
     print('There are {:d} supplier selected.'.format(int(quicksum(sol))))
     suppliers = [f'suppiler{i}' for i in np.where(sol)[0]]
